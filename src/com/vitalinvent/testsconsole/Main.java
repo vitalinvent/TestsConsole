7fa7e608188de1d5aa669843537c7a892f9aa7de
brezhnev <655350>
2019-11-29 16:43:33 +0300

    fffuibodachi this code trow error

--- src/com/vitalinvent/testsconsole/Main.java
+++ src/com/vitalinvent/testsconsole/Main.java
@@ -6,5 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         ContinuousIterator continuousIterator = new ContinuousIterator(Arrays.asList(1, 2, 3));
+        Object e0 = continuousIterator.next();
+        continuousIterator.setDone();
+        Object e2 = continuousIterator.next();
+        Object e3 = continuousIterator.next();
+        Object e4 = continuousIterator.next();
+        Object e5 = continuousIterator.next();
+
     }
 }
